5c4b56afae846d90647f0393863d8602edb6b8d6
deoramadanu <deo121005@gmail.com>
2025-05-06 12:13:20 +0700

    bool delNode

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -72,6 +72,18 @@ public:
         }
         return (*current != NULL);
     }
+    bool delNode (int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
 
+        if (current == START)
+            START = START ->next;
+        
+        delete current;
+        return true;
+    }
+    
     
         
