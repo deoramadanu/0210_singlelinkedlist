09f7cfb85c396ff37f69d9b7b9884b341bdf8d48
deoramadanu <deo121005@gmail.com>
2025-05-06 11:29:45 +0700

    perulangan if

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -27,5 +27,8 @@ public:
     
         Node *nodeBaru = new Node;
         nodeBaru-> noMhs = nim;
-
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            
         
