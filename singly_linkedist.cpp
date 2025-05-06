27f0e7a061a7254e06c27ecbd94caf3aadcf7610
deoramadanu <deo121005@gmail.com>
2025-05-06 11:29:03 +0700

    menampilkan masukan nomor mahasiswa

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -20,4 +20,12 @@ public:
     }
 
     void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
     
+        Node *nodeBaru = new Node;
+        nodeBaru-> noMhs = nim;
+
+        
