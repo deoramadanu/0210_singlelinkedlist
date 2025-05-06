5cab07e2611d5dc72b3021a40a477609ae19fc76
deoramadanu <deo121005@gmail.com>
2025-05-06 12:15:36 +0700

    menampilkan menu

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -103,6 +103,27 @@ public:
         }
     }
 };
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+    while(1)
+    {
+        cout << endl
+             << "Menu";
+        cout << endl
+             << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua dats didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+        
 
 
     
